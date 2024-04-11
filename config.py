cb8da0aeb53535d6e2e8b422b9c0c291598fe184
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2024-04-11 21:50:40 +0200

    add minimal bot for testing

---
+++ config.py
@@ -0,0 +1,4 @@
+from decouple import config
+
+#token
+TOKEN =config('TOKEN')
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="interaction_discord_bot",
-    version="0.1.4",
+    version="0.1.9",
     packages=find_packages(),
     install_requires=[
         "nextcord",
