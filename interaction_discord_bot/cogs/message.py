470195cafbd9b55f0d4b89f3cf2c8e6655cb0596
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2024-08-03 17:48:09 +0200

    chore: Add update_speak command and modal for message updates

--- interaction_discord_bot/cogs/message.py
+++ interaction_discord_bot/cogs/message.py
@@ -1,7 +1,7 @@
 from nextcord.ext import commands
 from nextcord import slash_command
 from interaction_discord_bot.modal.speakModal import SpeakModal
-
+from interaction_discord_bot.modal.updateModal import UpdateModal
 
 class Interaction(commands.Cog):
     """Message command for admin"""
@@ -24,6 +24,17 @@ class Interaction(commands.Cog):
             await interaction.response.send_modal(
                 SpeakModal(self.bot, interaction.channel.id)
             )
+
+    @slash_command(description="🔧🎙️", dm_permission=False, default_member_permissions=0)
+    async def update_speak(self, interaction, message_id: str):
+        """Update a message in a channel"""
+        try:
+            message = await interaction.channel.fetch_message(message_id)
+            await interaction.response.send_modal(
+                UpdateModal(self.bot, message)
+            )
+        except Exception as e:
+            await interaction.response.send_message(f"Error : {e}", ephemeral=True)
         
 def setup(bot):
     bot.add_cog(Interaction(bot))
