3ef2aadc8645ca1098b39d125d4656a6e243ab06
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-04-05 21:37:30 +0200

    format

--- bot.py
+++ bot.py
@@ -4,9 +4,11 @@ from interaction_discord_bot.init_cog import init_cog
 
 bot = commands.Bot()
 
+
 @bot.event
 async def on_ready():
-    print(f'{bot.user} is ready') 
+    print(f"{bot.user} is ready")
+
 
 init_cog(bot)
 
