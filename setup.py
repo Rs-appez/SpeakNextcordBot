db6572ac08cc1a33e3d4fd9250cc29408161b6d1
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-06-18 02:37:44 +0200

    add action add_vote

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="SpeakNextcordBot",
-    version="1.0.2",
+    version="1.1.0",
     packages=find_packages(),
     install_requires=[
         "nextcord",
